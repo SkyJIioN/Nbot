b3feab717f114027be1f7b4ac22c21b7433acca3
SkyJIioN <lnekrol@gmail.com>
2025-06-08 20:20:21 +0300

    Update market_data.py

--- services/market_data.py
+++ services/market_data.py
@@ -1,17 +1,12 @@
 import requests
 
-def get_crypto_prices(symbols=["bitcoin", "ethereum"]):
-    url = f"https://api.coingecko.com/api/v3/simple/price"
-    params = {
-        "ids": ",".join(symbols),
-        "vs_currencies": "usd"
-    }
-    response = requests.get(url, params=params)
-    data = response.json()
-
-    prices = {}
-    for symbol in symbols:
-        price = data.get(symbol, {}).get("usd", "н/д")
-        prices[symbol.upper()] = price
-
-    return prices
+def get_crypto_prices(ids):
+    url = "https://api.coingecko.com/api/v3/simple/price"
+    params = {"ids": ",".join(ids), "vs_currencies": "usd"}
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        data = response.json()
+        return {k: float(v["usd"]) for k, v in data.items()}
+    except Exception as e:
+        return {}
