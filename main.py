4e4d23d1f5e9c49f83a7b18d5721bbf8c76268b8
SkyJIioN <lnekrol@gmail.com>
2025-06-13 21:50:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 from fastapi import FastAPI, Request
-from telegram import Update, Bot
+from telegram import Update
 from telegram.ext import Application
-from webhook import application as app_telegram  # це ApplicationBuilder().token(...).build()
+from handlers.analyze import handle_symbol_input
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 app = FastAPI()
+
+app_telegram = Application.builder().token(BOT_TOKEN).build()
+app_telegram.add_handler(handle_symbol_input)
 
 @app.post("/webhook")
 async def webhook(request: Request):
-    try:
-        data = await request.json()
-        update = Update.de_json(data, app_telegram.bot)
-
-        # Ініціалізація додатку
-        if not app_telegram._initialized:
-            await app_telegram.initialize()
-            await app_telegram.start()
-
-        await app_telegram.process_update(update)
-        return {"ok": True}
-    except Exception as e:
-        print(f"Webhook error: {e}")
-        return {"ok": False, "error": str(e)}
+    data = await request.json()
+    update = Update.de_json(data, app_telegram.bot)
+    await app_telegram.initialize()
+    await app_telegram.process_update(update)
+    return {"ok": True}
