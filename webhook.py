4f6cf1e80f5548986dda17e09748fae29b5a5b4b
SkyJIioN <lnekrol@gmail.com>
2025-06-14 08:06:37 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,11 +1,11 @@
-# --- webhook.py ---
-from telegram.ext import Application, CommandHandler, MessageHandler, filters
-from handlers.analyze import analyze_command, handle_symbol_input
 import os
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from handlers.analyze import analyze_command, handle_symbol_input
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+application = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
 
-app_telegram = Application.builder().token(BOT_TOKEN).build()
+application.add_handler(CommandHandler("start", analyze_command))
+application.add_handler(CommandHandler("analyze", analyze_command))
+application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_symbol_input))
 
-app_telegram.add_handler(CommandHandler("start", analyze_command))
-app_telegram.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_symbol_input))
+webhook_handler = application
