5af681a469a91013ac538432de8d00a8af39b340
SkyJIioN <lnekrol@gmail.com>
2025-06-20 09:16:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from telegram.ext import CallbackQueryHandler
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, CallbackQueryHandler, filters
 from handlers.analyze import analyze_command, handle_symbol_input, handle_timeframe_selection
 import os
 
@@ -7,10 +6,6 @@ TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 app_telegram = ApplicationBuilder().token(TOKEN).build()
 
-# Обробник команди /analyze
 app_telegram.add_handler(CommandHandler("analyze", analyze_command))
-
-# Обробник введення назви монети
 app_telegram.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_symbol_input))
-
 app_telegram.add_handler(CallbackQueryHandler(handle_timeframe_selection, pattern="^tf_"))
