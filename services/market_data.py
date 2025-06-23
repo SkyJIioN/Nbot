d7a414bb4d0cf911f5b18bf85bf47b637aa93e9a
SkyJIioN <lnekrol@gmail.com>
2025-06-23 18:45:24 +0300

    Update market_data.py

--- services/market_data.py
+++ services/market_data.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd 
-import numpy as np from datetime 
-import datetime
+import numpy as np 
+from datetime import datetime
 
 API_KEY = "YOUR_CRYPTOCOMPARE_API_KEY"
 
