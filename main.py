e385fdd36b943d4373f72f4c478743ec65260caa
SkyJIioN <lnekrol@gmail.com>
2025-06-29 19:53:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 from fastapi import FastAPI
 from handlers.scan import scan_command
 from webhook import webhook_router
-from app import app_telegram  # Імпортуємо Application
+from app import app_telegram
+from telegram.ext import CommandHandler
 import asyncio
 
 app = FastAPI()
 
 @app.on_event("startup")
 async def on_startup():
-    # 🔄 Ініціалізуємо Telegram Application при запуску FastAPI
     await app_telegram.initialize()
+    app_telegram.add_handler(CommandHandler("scan", scan_command))  # ✅
     print("✅ Telegram Application initialized")
 
-application.add_handler(CommandHandler("scan", scan_command))
-
-# Підключаємо маршрут вебхука
 app.include_router(webhook_router)
 
 @app.get("/")
