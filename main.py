164e729108e25339de5f8101a749b99f7ce0b3e7
SkyJIioN <lnekrol@gmail.com>
2025-07-04 21:49:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,16 @@ from handlers.scan import scan_command
 from webhook import webhook_router
 from app import app_telegram
 from telegram.ext import CommandHandler
-import os
 
 app = FastAPI()
 
 @app.on_event("startup")
 async def on_startup():
-    # Додаємо хендлер
-    app_telegram.add_handler(CommandHandler("scan", scan_command))
-
-    # Ініціалізуємо Application
     await app_telegram.initialize()
+    app_telegram.add_handler(CommandHandler("scan", scan_command))
+    print("✅ Telegram Application initialized & handler registered")
 
-    # Встановлюємо Webhook
-    webhook_url = os.getenv("WEBHOOK_URL")
-    if not webhook_url:
-        raise ValueError("WEBHOOK_URL is not set!")
-    await app_telegram.bot.set_webhook(url=webhook_url + "/webhook")
-    print(f"✅ Webhook встановлено: {webhook_url}/webhook")
-
-# Підключаємо маршрут
+# Підключаємо маршрут вебхука
 app.include_router(webhook_router)
 
 @app.get("/")
