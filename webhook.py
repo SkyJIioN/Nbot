9d34c53e3a51004b788989f84050862642e475cb
SkyJIioN <lnekrol@gmail.com>
2025-07-10 20:16:46 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,27 +1,22 @@
 from fastapi import APIRouter, Request
 from telegram import Update
-from app import app_telegram  # Telegram Application
-import asyncio
+
+from app import app_telegram  # Імпортуємо Application
 
 webhook_router = APIRouter()
+
 
 @webhook_router.post("/webhook")
 async def webhook_handler(request: Request):
     try:
         data = await request.json()
-        print("🔔 Отримано новий update:", data)  
         update = Update.de_json(data, app_telegram.bot)
 
-        # Ініціалізація Telegram Application (тільки 1 раз)
         if not app_telegram._initialized:
-            await app_telegram.initialize()
+            await app_telegram.initialize()  # <== ДОДАНО ЦЕ
 
-        # 🧠 Обробка апдейту у фоновому режимі (асинхронний task)
-        asyncio.create_task(app_telegram.process_update(update))
-
-        # ✅ Відповідаємо Telegram швидко
+        await app_telegram.process_update(update)
         return {"status": "ok"}
-
     except Exception as e:
         print(f"Error handling webhook: {e}")
         return {"status": "error", "message": str(e)}
